26a9d8a6783249dabf4dc7a88c7f18b156a7c156
DisCartion <1138018467@qq.com>
2021-06-28 23:30:39 +0800

    更新

--- xinyu/js/lovetime.js
+++ xinyu/js/lovetime.js
@@ -21,7 +21,7 @@ function lovetime() {
     var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes)
     var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes *
         minutes) / seconds)
-    document.getElementById("lovetime").innerHTML = "我们已经认识 " + diffYears + "年" + diffDays + "天" +
+    document.getElementById("lovetime").innerHTML = "我已经喜欢玉玉 " + diffYears + "年" + diffDays + "天" +
         diffHours + "小时" + diffMinutes + "分钟" + diffSeconds + "秒啦"
 }
 lovetime()
